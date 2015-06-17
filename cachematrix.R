566b18553b700c1fb32ba6a42077010893acdc5a
Anastasiarep <an.repouliou@yahoo.gr>
2015-06-17 13:33:41 +0300

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that stores a list
+## of functions, creating a special "matrix" object
+## that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+        s <- NULL
+        set <- function(y)
+## changes the matrix solved in the main function
+	{
+                x <<- y
+                s <<- NULL
+        } 
+        get <- function() x
+## returns the vector x stored in the main function
+        setsolve <- function(inverse) s <<- inverse
+## stores the value of the input in a variable s
+## inverse is supposed to be the inverse of matrix x
+        getsolve <- function() s
+## returns the value of the variable s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+## binds all four functions into a list
+## when we assign makeCacheMatrix to an object, it has all the functions
 }
 
+## cacheSolve is a function that computes the inverse of the
+## special "matrix" returned by makeCacheMatrix. If the inverse
+## has already been calculated (and the matrix has not
+## changed), it retrieves the inverse from the cache,
+## rather than recalculate it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+        s <- x$getsolve()
+        if(!is.null(s))
+## verifies the value s axists and is not NULL
+	{
+                message("getting cached data")
+                return(s)
+        }
+## if s exists in memory, returns a message and the value s
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
+## else, gets the matrix stored,
+## calculates the inverse
+## and stores it in the object generated
 }
